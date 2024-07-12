9e6848b3bc407d06d9a2cbbe9aa48df9729a4159
Realta Scope Tech <82439057+RealtaScopeTech@users.noreply.github.com>
2024-07-12 20:33:41 +0100

    Corrected registering and unregistering 64bit dll

--- ASCOM driver/InstallDriver.bat
+++ ASCOM driver/InstallDriver.bat
@@ -2,7 +2,7 @@ if not exist "C:\RealtaScopeTech\" mkdir C:\RealtaScopeTech
 
 xcopy "%~dp0" C:\RealtaScopeTech /y
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe /codebase "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll" runhidden 32bit
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe /codebase "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll" runhidden 64bit
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe /codebase "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll"
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /codebase "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll"
 
 timeout 200
--- ASCOM driver/UnistallDriver.bat
+++ ASCOM driver/UnistallDriver.bat
@@ -1,6 +1,6 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe -u "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll" runhidden 32bit
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe /codebase "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll" runhidden 64bit
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe -u "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll" runhidden 64bit
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe -u "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll" 
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe /codebase "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll"
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe -u "C:\RealtaScopeTech\ASCOM.EBB.Focuser.dll"
 
 @RD /S /Q "C:\RealtaScopeTech"
 
