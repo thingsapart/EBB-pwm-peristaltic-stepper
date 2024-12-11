8ba2321ebbdeabfaba148be3c662170238ba4e9f
thingsapart <thingstakenapart@gmail.com>
2024-12-10 16:36:49 -0800

    Fix and cleanup the code a little, use VACTUAL for setting velocity

--- Arduino/EBBTelescopeFocuser/EBBTelescopeFocuser.ino
+++ Arduino/EBBTelescopeFocuser/EBBTelescopeFocuser.ino
@@ -1,26 +1,40 @@
+#define FLASH_BANK_NUMBER FLASH_BANK_1
+
 #include <EEPROM.h>;
 #include <SoftwareSerial.h>
 #include "MotorClass.h";
 #include "SerialComms.h";
 
+// PINS: Motor.
 #define D_STEP_PIN PD0
 #define D_UART_PIN PA15
 #define D_ENABLE_PIN PD2
-#define D_HEATER_PIN PB13
-#define D_PWM_INPUT_PIN PB7
+#define D_DIR_PIN PD1
 
-#define D_DRIVER_ADDRESS 0b00 // PCB has both TMC2209 MS1 and MS2 pins grounded so this is the correct address for that configuration
-#define D_R_SENSE 0.11f // These are the resistance values of the current monitoring resistors as per the PCB datasheet, it has two 0R11 resistors (0.11 Ohm). This is used by the TMC2209 stepper driver to measure the current going to the stepper motor. It has one resistor for each coil of the stepper. 
+// PINS: PWM input (PB9 on aux plug eg).
+#define D_PWM_INPUT_PIN PB9
+
+// CONFIG: board/hardware.
+#define D_DRIVER_ADDRESS 0b00         // PCB has both TMC2209 MS1 and MS2 pins grounded so this is the correct address for that configuration
+#define D_R_SENSE 0.11f               // These are the resistance values of the current monitoring resistors as per the PCB datasheet, it has two 0R11 resistors (0.11 Ohm). This is used by the TMC2209 stepper driver to measure the current going to the stepper motor. It has one resistor for each coil of the stepper. 
  
+// SETUP:
+#define MOTOR_CURRENT 1000
+#define MOTOR_MAX_RPS 4.0f            // Motor revolutions per second of the motor, float.
+#define MOTOR_STEPS_PER_REV 200       // Motor full steps per revolution (= 360 / motor-degrees-per-step), 1.8* ~ 200, 0.9* ~ 400.
+#define MOTOR_MICROSTEPS 16           // Default microsteps, can be overridden if serial G-Codes and EEPROM are enabled.
+
+#define MOTOR_DIRECTION LOW           // flip value to HIGH/LOW to reverse motor direction, direction depends on motor wiring.
+
+// CONFIG: Features.
+// #define DEBUG                      // Debug output on serial port.
+#define SERIAL_GCODE                  // G-Code config inherited from Realta Ebb Telescope Focuser - allows setting current and other useful features but requires EEPROM.
+// #define EEPROM                     // Needs modification of board config to allow enabling the feature.
+
 SoftwareSerial G_TMC_SERIAL (D_UART_PIN, D_UART_PIN);
 Motor MyMotor(&G_TMC_SERIAL, D_R_SENSE, D_DRIVER_ADDRESS);
 
-const unsigned long TIMEOUT = 1000; // 1 millisecond (1000 Hz) 
-
-const byte PWMPin = 3;
-const byte InputPin = 4;
-bool led = false;
-unsigned int counter = 0;
+const unsigned long TIMEOUT = 1000; // 1 millisecond (100 Hz) 
 
 // https://forum.arduino.cc/t/to-read-pwm-signal-with-pulsein/930016/5
 int readPWM(int pin)
@@ -28,10 +42,12 @@ int readPWM(int pin)
   unsigned long highDuration = pulseIn(pin, HIGH, TIMEOUT); 
   unsigned long lowDuration = pulseIn(pin, LOW, TIMEOUT);
 
-  // Serial.print(highDuration);
-  // Serial.print(", ");
-  // Serial.print(lowDuration);
-  // Serial.print(" -> ");
+#if DEBUG
+  Serial.print(highDuration);
+  Serial.print(", ");
+  Serial.print(lowDuration);
+  Serial.println(" -> ");
+#endif
 
   if (highDuration == 0 || lowDuration == 0)
     return digitalRead(pin) == HIGH ? 255 : 0;
@@ -39,31 +55,53 @@ int readPWM(int pin)
   return (highDuration * 255) / (highDuration + lowDuration);
 }
 
+// Current version of settings, change whenever slots change or setting is added.
+#define SETTINGS_VER 9 
+
+void reset_eeprom_to_defaults () {
+  EEPROM.put(1, 0); // Current position.
+  EEPROM.put(10, MOTOR_CURRENT); // Motor Current.
+  EEPROM.put(20, MOTOR_MICROSTEPS); // Micro Steps.
+  EEPROM.put(30, MOTOR_MAX_RPS); // Default max rps.
+  EEPROM.put(0, SETTINGS_VER); 
+}
+
 void setup() {
+#if defined(EEPROM) && defined(FLASH_BANK_NUMBER)
   // put your setup code here, to run once:
   byte isFirstRun;
   EEPROM.get(0, isFirstRun);
 
-  if (isFirstRun != 6)
-  {
-    EEPROM.put(1, 50000); // Current position
-    EEPROM.put(10,500); // Motor Current
-    EEPROM.put(20,8); // Micro Steps
-    EEPROM.put(0,6); 
+  if (isFirstRun != SETTINGS_VER) {
+    reset_eeprom_to_defaults();
   }
   
+  // Apply values from EEPROM.
   EEPROM.get(1, MyMotor.CurrentPosition);
   EEPROM.get(10, MyMotor.current);
   EEPROM.get(20, MyMotor.steps);
-  
+  EEPROM.get(30, MyMotor.max_rps);
+#else
+  MyMotor.steps = MOTOR_MICROSTEPS;
+  MyMotor.current = MOTOR_CURRENT;
+  MyMotor.CurrentPosition = 0;
+  MyMotor.max_rps = MOTOR_MAX_RPS;
+#endif
+
   MyMotor.IsMoving = false;
   pinMode(D_STEP_PIN, OUTPUT);
   pinMode(D_ENABLE_PIN, OUTPUT);
+  pinMode(D_DIR_PIN, OUTPUT);
+
+  // Sanity check current values and reset to default if out of range.
+  if (MyMotor.current < 200 || MyMotor.current > 1500) { MyMotor.current = MOTOR_CURRENT; }
 
   MyMotor.engageMotor(true);
   MyMotor.setHeaterPWM(0);
   
-  Serial.begin(9600); // Start serial communication to PC
+  // Start serial communication to PC.
+  Serial.begin(9600);
+  Serial.println("Serial started");
 
   // Set up PWM input pin.
   pinMode(D_PWM_INPUT_PIN, INPUT);
@@ -71,19 +109,37 @@ void setup() {
   // LED.
   pinMode(LED_BUILTIN, OUTPUT);
 
-  G_TMC_SERIAL.begin(11520);  // Start serial communication with TMC
+  // Start serial communication with TMC.
+  G_TMC_SERIAL.begin(11520);
   MyMotor.beginSerial(11520); 
   MyMotor.begin();                                                                                                                                                                                                                                                                                                                            // UART: Init SW UART (if selected) with default 115200 baudrate
   MyMotor.toff(5);                          // Enables driver in software
   MyMotor.rms_current(MyMotor.current);     // Set motor RMS current, needs to be user configurable, unit is mA
   MyMotor.microsteps(MyMotor.steps);        // Set the micro steps of the motor driver
 
-  MyMotor.en_spreadCycle(true);             // sets stepper to use silent mode
-  MyMotor.pwm_autoscale(true);              // Needed for stealthChop
+  MyMotor.en_spreadCycle(true);             // sets stepper to use spreadcycle mode for more torque.
+  // MyMotor.pwm_autoscale(true);           // Needed for stealthChop
+
+  // Set direction.
+  digitalWrite(D_DIR_PIN, MOTOR_DIRECTION);
 }
 
 void loop() {
-   
+  int duty_cycle = readPWM(D_PWM_INPUT_PIN);
+
+  if (duty_cycle > 0) {
+    // Compute motor velocity from PWM duty_cycle, steps per revolution, microsteps and maximum motor revs/sec.
+    MyMotor.VACTUAL((float) duty_cycle / 255.0f / 0.715f * MOTOR_STEPS_PER_REV * MyMotor.steps * MyMotor.max_rps);
+  } else {
+    MyMotor.engageMotor(false);
+  }
+
+#ifdef DEBUG  
+  Serial.print("duty cycle: ");
+  Serial.println(duty_cycle);
+#endif
+
+#ifdef SERIAL_GCODE
    // Check the serial port for command
    P_PROCESS_SERIAL_PORT();
 
@@ -97,44 +153,40 @@ void loop() {
    if (G_END_COMMAND_FOUND)
    {
       G_END_COMMAND_FOUND = false;
-      // Code goes here
+
+      // G1: Set Motor max RPS.
       if(G_COMMAND == "G1")
       {
-        MyMotor.SetMoveTarget(G_PARAMS.toInt());
+        MyMotor.max_rps = G_PARAMS.toFloat();
       }
-
+      // G2: Halt motor.
       if(G_COMMAND == "G2")
       {
         MyMotor.Halt();
       }
-      
+      // G3: Returns true (1) or false (0) depending on if the motor is moving or not.
       if(G_COMMAND == "G3")
       {
         Serial.print(String(MyMotor.IsMoving) + "#");
       }
-
+      // G4: Returns the current maximum revs/second.
       if(G_COMMAND == "G4")
       {
-        Serial.print(String(MyMotor.CurrentPosition) + "#");
+        Serial.print(String(MyMotor.max_rps) + "#");
       }
-
-      if(G_COMMAND == "G5")
-      {
-        MyMotor.CurrentPosition = G_PARAMS.toInt();
-      }
-
+      // G6: Sets motor current in milli amps.
       if(G_COMMAND == "G6")
       {
         MyMotor.current = G_PARAMS.toInt();
         EEPROM.put(10,MyMotor.current);
         MyMotor.rms_current(MyMotor.current);
       }
-
+      // G7: Returns current motor current setting in milli amps.
       if(G_COMMAND == "G7")
       {
         Serial.print(String(MyMotor.current) + "#");
       }
-
+      // G8: Sets motor drivers number of micro steps: 0, 8, 16, 32, 64,128, 256.
       if(G_COMMAND == "G8")
       {
         if(G_HAS_PARAMS)
@@ -144,22 +196,22 @@ void loop() {
           MyMotor.microsteps(MyMotor.steps);
         }
       }
-
+      // G9: Returns current number of micro steps.
       if(G_COMMAND == "G9")
       {
         Serial.print(String(MyMotor.steps) + "#");
       }
-
+      // G10: Sets heater PWM value.
       if(G_COMMAND == "G10")
       {
         MyMotor.setHeaterPWM(G_PARAMS.toInt());
       }
-
+      // G11: Returns current heater PWM value.
       if(G_COMMAND == "G11")
       {
         Serial.print(String(MyMotor.CurrentHeaterValue) + "#");     
       }
-
+      // G12: Engage motor, true/false.
       if(G_COMMAND == "G12")
       {
         boolean trueFalse;
@@ -174,7 +226,7 @@ void loop() {
         
         MyMotor.engageMotor(trueFalse);
       }
-
+      // G13: Returns motor engaged state, true or false.
       if(G_COMMAND == "G13")
       {
         if(MyMotor.IsEngaged)
@@ -186,43 +238,13 @@ void loop() {
           Serial.print("0#");
         }
       }
-
+      // G14: Resets saved settings to default.
       if(G_COMMAND == "G14")
       {
-        EEPROM.put(1, 50000); // Current position
-        EEPROM.put(10,500); // Motor Current
-        EEPROM.put(20,8); // Micro Steps
-        // read these back out into motor classes variables
-        EEPROM.get(1, MyMotor.CurrentPosition);
-        EEPROM.get(10, MyMotor.current);
-        EEPROM.get(20, MyMotor.steps);
         // send some of these settings to the motor driver
         MyMotor.rms_current(MyMotor.current);     // Set motor RMS current, needs to be user configurable, unit is mA
         MyMotor.microsteps(MyMotor.steps);  // Set the micro steps of the motor driver
       }
    }
-  
-  // HACK until proper rewrite: Keep rolling the target forward, effectively one microstep per PWM signal.
-  MyMotor.SetMoveTarget(MyMotor.MoveTarget + 1);
-  int duty_cycle = readPWM(D_PWM_INPUT_PIN);
-
-  if (counter++ % 100 == 0) {
-    Serial.print("Duty cycle: ");
-    Serial.print(duty_cycle);
-    Serial.print(", ");
-    Serial.println(counter);
-    
-    digitalWrite(LED_BUILTIN, led ? 255 : 0);
-    led = !led;
-  }
-
-  // put your main code here, to run repeatedly:
-  if(MyMotor.IsMoving == true)
-  {
-     if(MyMotor.Move() == true)
-     {
-      // Change direction of TMC2209 driver to match MyMotor.move_direction//
-      // Raise and lower stepper motor pin // 
-     }
-  }
+#endif 
 }
--- Arduino/EBBTelescopeFocuser/MotorClass.h
+++ Arduino/EBBTelescopeFocuser/MotorClass.h
@@ -20,83 +20,22 @@ class Motor: public TMC2209Stepper
       byte CurrentHeaterValue;
       boolean IsMoving;
       boolean IsEngaged;
-      long CurrentPosition;
-      long MoveTarget;
+      unsigned long CurrentPosition;
+      unsigned long MoveTarget;
       boolean move_direction;
       int current;
       int steps;
+      float max_rps;
 };
 
 Motor::Motor(Stream * SerialPort, float RS, uint8_t addr): TMC2209Stepper(SerialPort,  RS, addr) 
 {
 }
 
-
-
 void Motor::Halt()
 {
      IsMoving = false;
      EEPROM.put(1, CurrentPosition);
-}
-
-void Motor::SetMoveTarget(long Position)
-{
-  if (Position == LONG_MAX) {
-    long diff = LONG_MAX - CurrentPosition;
-    Position = LONG_MIN + diff;
-    CurrentPosition = LONG_MIN;
-  }
-
-  if(CurrentPosition != Position)
-  {
-     IsMoving = true;
-     MoveTarget = Position;
-  }
-}
-
-boolean Motor::Move()
-{
-          long difference;
-          boolean new_direction;
-          difference = MoveTarget - CurrentPosition;
-
-          if(!IsEngaged)
-          {
-            engageMotor(true);
-          }
-          
-          if(difference == 0)
-          {
-            Halt();
-            return false;
-          }
-          else
-          {
-            if(difference > 0)
-            {
-              new_direction = true;
-              CurrentPosition = CurrentPosition + 1;
-            }
-            else
-            {
-              new_direction = false;
-              CurrentPosition = CurrentPosition - 1;
-            }
-
-            if(new_direction != move_direction)
-            {
-              move_direction = new_direction;
-              shaft(move_direction);   
-            }
-            
-            digitalWrite(D_STEP_PIN, HIGH); // Raise voltage on this pin to VCC or 3.3v
-            delayMicroseconds(120); // wait
-            digitalWrite(D_STEP_PIN, LOW);
-            delayMicroseconds(120); // wait
-
-            return true;
-            
-          }
 }
 
 void Motor::setHeaterPWM(byte HeaterValue)
@@ -108,12 +47,10 @@ void Motor::setHeaterPWM(byte HeaterValue)
 void Motor::engageMotor(boolean Engage)
 {
   IsEngaged = Engage;
-  if(Engage)
-  {
-  digitalWrite(D_ENABLE_PIN, LOW);
+  if(Engage) {
+    digitalWrite(D_ENABLE_PIN, LOW);
   }
-  else
-  {
-  digitalWrite(D_ENABLE_PIN, HIGH);
+  else {
+    digitalWrite(D_ENABLE_PIN, HIGH);
   }
 }
